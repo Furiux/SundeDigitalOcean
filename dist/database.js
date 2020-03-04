b71d15f02c4117d221228c848ad6008f53cd1b11
Nuñez Ezequiel Antonio <enunez@agdcorp.com.ar>
2020-03-04 09:46:16 -0300

    primer commit

---
+++ dist/database.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(db => console.log('DB is connect')).catch(error => console.log(error));
